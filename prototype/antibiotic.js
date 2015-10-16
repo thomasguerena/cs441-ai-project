7309ef11f88f1523f0ed05cdeca3f20b1b693bd8
Thomas Guerena <guerena.t@gmail.com>
2015-10-15 17:50:29 -0700

    Fixed divide-by-zero error in antibiotic fitness function.

--- prototype/antibiotic.js
+++ prototype/antibiotic.js
@@ -2,19 +2,24 @@
 	'use strict';
 
 	window.Antibiotic = function () {
-		// TODO - randomaly assign a new center
 		this.center = 2048; // ~ middle of 0 and 4906;
-		this.leniency = 64;
+		this.leniency = 64; // uneffective range
 	};
 
 	Antibiotic.prototype.chanceOfSurvival = function(cell) {
 		var c = this.center;
-    	var l = this.leniency;
+    	var l = Math.max(this.leniency - 2*Math.floor(generation/10), 0);
     	var x = strToHex(cell.dna); // dna as decimal value
     	var offset = Math.abs(c - x); // distance from center
     	offset = Math.min(offset, l); // reduce offset
-    	offset = offset / l; // scale offset
-    	offset = Math.min(offset, 1); // limit offset
-    	return Math.max(0.1, 1 - offset); // propability of survival = 1 - fitness
+    	if (offset > 0) {
+    		offset = offset / l; // scale offset
+    		offset = Math.min(offset, 1); // limit offset
+    		return Math.max(0.1, 1 - offset); // propability of survival
+    	} else {
+    		return x === c ? 1 : 0.1;
+    	}
+
+
 	};
 })();
